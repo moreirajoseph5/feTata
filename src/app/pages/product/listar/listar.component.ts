a6c9f6ecd09d1af84ba27d03053d5ba9b1c1a6ab
Joseph Moreira <moreirajoseph5@gmail.com>
2025-06-30 00:48:25 -0500

    Filtro

--- src/app/pages/product/listar/listar.component.ts
+++ src/app/pages/product/listar/listar.component.ts
@@ -1,14 +1,26 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductService} from '../../../shared/services/product.service';
+import {ProductResponse} from '../../../shared/interfaces/product-response.inteface';
+import {NgForOf, NgIf} from '@angular/common';
+import {Product} from '../../../shared/interfaces/products.interface';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-listar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf,
+    FormsModule
+  ],
   templateUrl: './listar.component.html',
   styleUrl: './listar.component.css'
 })
 export class ListarComponent implements OnInit {
+  allProductos!:ProductResponse;
+  filteredProductos: Product[] = [];
+  searchText: string = '';
+  
 
   constructor(private producService: ProductService) {
   }
@@ -19,7 +31,8 @@ export class ListarComponent implements OnInit {
   getProductos(): void {
     this.producService.getProducts().subscribe({
       next: (productos) => {
-        console.log(productos);
+        this.allProductos = productos;
+        this.filteredProductos = productos.data;
       },
       error: (err) => {
         console.error('Error al obtener productos:', err);
@@ -27,4 +40,12 @@ export class ListarComponent implements OnInit {
     });
   }
 
+  filterProductos(): void {
+    const search = this.searchText.toLowerCase().trim();
+    this.filteredProductos = this.allProductos.data.filter(product =>
+      Object.values(product).some(val =>
+        String(val).toLowerCase().includes(search)
+      )
+    );
+  }
 }
